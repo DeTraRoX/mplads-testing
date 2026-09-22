802fc2c8fe6bb0a38c11a5f1edcbf2a85a81fe4c
DeTraRoX <ayushdobhal850@gmail.com>
2026-09-23 00:11:39 +0530

    fixed minor bugs

--- mplads-ai-platform-main/backend/app/main.py
+++ mplads-ai-platform-main/backend/app/main.py
@@ -54,7 +54,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://mplads-testing-1.onrender.com
+    "https://mplads-testing-1.onrender.com"
 ]
 
 app.add_middleware(
